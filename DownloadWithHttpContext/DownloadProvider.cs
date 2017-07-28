f381cd51d5fab152d13b7d217fbfbe9c8a51f583
mandy <1140675478@qq.com>
2017-07-28 17:12:02 +0800

    update download with httpcontext

---
+++ DownloadWithHttpContext/DownloadProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DnsRmms.Services.Provider
+{
+    public class DownloadProvider : IHttpHandler
+    {
+        public DownloadProvider(string filePath, string mimeType==null)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                throw new ArgumentException("download fail , the path is empty");
+
+            FilePath = filePath;
+            mimeType = mimeType ?? "text/plain";
+        }
+        /// <summary>
+        /// 绝对路径
+        /// </summary>
+        public string FilePath { get; set; }
+
+        public string MimeType { get; set; }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpResponse response = HttpContext.Current.Response;
+            response.ClearContent();
+            response.Clear();
+            response.ContentType = MimeType;
+            response.TransmitFile(FilePath);
+            response.AddHeader("Content-Disposition", "attachment;filename=" + Path.GetFileName(FilePath) + ";");
+            response.Flush();
+            response.End();
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+    }
+}
